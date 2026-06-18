2ab88f271eb38430a5b482a4099661f12790f1da
Robin Khatri <rkhatri@realists.de>
2026-06-18 14:12:49 +0200

    finished everything till animation

--- Model/ModelData.swift
+++ Model/ModelData.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+
+@Observable
+class ModelData {
+    var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+}
 
 
 func load<T: Decodable>(_ filename: String) -> T {
@@ -15,8 +20,8 @@ func load<T: Decodable>(_ filename: String) -> T {
 
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
 
