b3afcb63ae4d3f872435a62ff78c86b6e6d0c4e9
Robin Khatri <rkhatri@realists.de>
2026-06-22 15:22:51 +0200

    Finished until working with UI Controls 15:22

---
+++ hello/egrw.swift
@@ -0,0 +1,8 @@
+//
+//  egrw.swift
+//  Landmarks
+//
+//  Created by rkhatri on 22.06.26.
+//
+
+import Foundation
---
+++ hello/fr.swift
@@ -0,0 +1,18 @@
+//
+//  fr.swift
+//  Landmarks
+//
+//  Created by rkhatri on 22.06.26.
+//
+
+import SwiftUI
+
+struct fr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fr()
+}
