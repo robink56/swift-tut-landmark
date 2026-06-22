93122e24aac23716f7b4086125cfa584efb52bde
Robin Khatri <rkhatri@realists.de>
2026-06-22 12:41:55 +0200

    Done until composing complex interface

---
+++ View/LandmarkDetail.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+
+            landmark.image
+                .resizable()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+
+                Divider()
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: ModelData().landmarks[0])
+}
